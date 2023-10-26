1615c6b057d9a54386de8919d080d947fa3cf4e0
Osmedi <140572467+Osmedi@users.noreply.github.com>
2023-10-26 13:29:14 -0600

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+001 //Ficha 4b
+002 /Sentencia tipo switch/
+003 #include <iostream.h>
+004 int main (void)
+005 {
+006 int i=1;
+007 switch (i)
+008 {
+009 case 1:
+010 cout <<"i es 1"<<endl;
+011 break;
+012 case 2:
+013 cout <<"i es 2"<<endl;
+014 break;
+015 case 25:
+016 cout << “i es 25“ << endl;
+017 default:
+018 cout <<"i no es ni 1 ni 2"<<endl;
+019 break;
+020 }
+021 return 0;
+022 }
