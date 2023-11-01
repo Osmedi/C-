f6825ae7c0eb9e1c8be2cf33cf4f4b4bca1e0f43
Osmedi <140572467+Osmedi@users.noreply.github.com>
2023-11-01 13:56:51 -0600

    Actualizar 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,6 +3,9 @@
 #define MAX_SIZE 25
 int main (void)
 {
+ int Matrix [MAX_SIZE];
+ // esto es un vector, se ve con detalle en las fichas 12
+// de momento lo tomamos como una definición para poder trabajar
  int size;
  ofstream MyReport("resultado.txt");
  cout << “Introduzca la dimensión del vector"<<endl;
@@ -10,18 +13,12 @@ int main (void)
  if (size > MAX_SIZE)
 {
  cerr << "Error, tamaño demasiado grande" << endl;
- MyReport << " ************* inicio" << endl;
+ MyReport << " *********************************** inicio"
+ << endl;
  MyReport << " ERROR: dimension del vector debe ser menor de "
  << MAX_SIZE << endl;
- MyReport << " ************** fin" << endl;
+ MyReport << " ************************************** fin"
+ << endl;
  MyReport.close();
  return 1;
-}
- cout << "Vamos a trabajar con un vector de " << size
- << " componentes" <<endl;
- MyReport << " ************* inicio" << endl;
- MyReport << " Vector de " << size << " componentes" << endl;
- MyReport << " ************** fin" << endl;
- MyReport.close();
- return 0;
 }
