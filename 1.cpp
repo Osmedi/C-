4cb8e83394fe05961d89fadf35ef6ba1e9b477f4
Osmedi <140572467+Osmedi@users.noreply.github.com>
2023-11-01 14:10:43 -0600

    Actualizar 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,15 +1,23 @@
-for (int i=0; i<size; i++) Matrix[i]=0;
- // inicializando a cero cada componente
- cout << "Vamos a trabajar con un vector de " << size
- << " componentes" <<endl;
- MyReport << " *********************************** inicio" << endl;
- MyReport << " Vector de " << size << " componentes" << endl;
- i=0;
- do { MyReport << Matrix[i] << " "; } while (++i<size);
- // atencion a la precedencia de operadores
- // el resultado es distinto si while (i++<size)
- MyReport << endl;
- MyReport << " ************************************** fin" << endl;
- MyReport.close();
- return 0;
-}
+ // Ficha 6
+002 /* funciones */
+003 #include <iostream.h>
+004 int checking (int i, int j); //declaración función (prototipo)
+005 int main (void) //programa principal
+006 {
+007 int i;
+008 cout<< "escribe un numero"<<endl; cin>>i;
+009 int j;
+010 cout<<"escribe otro numero"<<endl;cin>>j;
+011 if (!checking(i,j)) //llamada a la función 
+//(si la función retorna 0)
+012 cout<<i<<" es mayor que "<<j<<endl;
+013 else //(si la función retorna 1)
+014 cout<<i<<" es menor que "<<j<<endl;
+015 return 0;
+016 }
+017 int checking (int i, int j) //definición de la función 
+//(implementación)
+018 {
+019 if (i<j) return 1;
+020 return 0;
+021 }
